854934957fbb64ea8c5ccc20b8b256816ecedcff
Petr Geršl <pgersl05@gmail.com>
2023-12-31 00:26:44 +0100

    imporved filtering

--- static/js/filter.js
+++ static/js/filter.js
@@ -1,8 +1,16 @@
 const filter = document.getElementById('filter')
 const filterItems = document.querySelectorAll('.filter-item')
+const headings = document.querySelectorAll('.filter-item-heading')
 
 filter.addEventListener('input', (e) => {
     const val = e.target.value.toLowerCase().trim();
+    headings.forEach(heading => {
+        if (!(val === '')){
+            heading.style.display = 'none'
+        } else {
+            heading.style.display = ''
+        }
+    })
     filterItems.forEach(filterItem => {
         const title = filterItem.querySelector('.filter-item-title').textContent.toLowerCase()
         if (!(title.includes(val))) {
@@ -10,7 +18,9 @@ filter.addEventListener('input', (e) => {
         } else {
             filterItem.style.display = ''
         }
-        if (val === '')
+        if (val === '') {
             filterItem.style.display = ''
+            headings.style.display = ''
+        }
     })
 })
